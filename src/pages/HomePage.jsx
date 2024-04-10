c8eccf967d5000f7688cbe8c865ac1e4137b5234
jokertitcho <jokertitcho@yahoo.fr>
2024-04-10 17:29:09 +0200

    sjflkds

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+
+const HomePage = () => {
+    return (
+        <div className="home">
+            <Header/>
+        </div>
+    );
+};
+
+export default HomePage;
